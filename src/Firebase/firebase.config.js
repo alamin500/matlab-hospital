53f02209972eab67558530b63da5068272ac5ac4
alamin500 <nalamin500@gmail.com>
2021-10-13 09:55:02 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARf812mvODr0-tM5VZ260PzGpHd1BltLU",
+  authDomain: "ema-john-simple-5ed4b.firebaseapp.com",
+  projectId: "ema-john-simple-5ed4b",
+  storageBucket: "ema-john-simple-5ed4b.appspot.com",
+  messagingSenderId: "9557165797",
+  appId: "1:9557165797:web:02b2303b2516669ec5b60c",
+};
+
+export default firebaseConfig;
