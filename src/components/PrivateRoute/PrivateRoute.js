2b6a73be40b0e6233ba798e940e68b712e229206
alamin500 <nalamin500@gmail.com>
2021-10-14 00:13:32 +0600

    h

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Redirect, Route } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
-const PrivateRoute = () => {
-  return <div></div>;
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user } = useAuth();
+
+  return (
+    <Route>
+      {...rest}
+      render=
+      {({ location }) =>
+        user.email ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          ></Redirect>
+        )
+      }
+    </Route>
+  );
 };
 
 export default PrivateRoute;
